5508745090cd0c5fd49dfc5760162bc1c963b520
Ahmed Helmy <117530532+Ahmedhelmy006@users.noreply.github.com>
2026-05-16 14:01:32 +0300

    Excerises to understand regex. Switch the variable with regex pattern to get a better grasp on how it works

--- understanding_regex.py
+++ understanding_regex.py
@@ -1,1 +1,35 @@
-print("Hello World!")
+import sys, re
+
+#pattern = r"^(.)(.)(.).?\3\2\1$"
+pattern = r"(.)"
+input("Hello. Today you'll be learning regex! Excited? \n")
+print("Whatever, Let's begin. This is the first example: \n")
+score = 0
+print(pattern)
+
+print("\nYour if your next input matches the pattern, it will print True. and you will get a score of 1. " \
+      "\nIf it doesn't, it will print False and your score will be deducted one from it. If you get 5 you win, if you get -5 you lose! " \
+      "\nBe a good boy and win!\n")
+
+while True:
+    user_input = input("What's your answer?")
+    if user_input == "\q":
+        break
+    if score == 5:
+        print("Goodboy! You win")
+        break
+    if score == -5:
+        print("Not a very good boy apparently...")
+        break
+    else:
+        if re.match(pattern=pattern, string=user_input):
+            print(True)
+            score +=1 
+            print(f"Your score is {score}")
+            continue
+
+        if not re.match(pattern=pattern, string=user_input):
+            print(False)
+            score -=1
+            print(f"Your score is {score}")
+            continue
